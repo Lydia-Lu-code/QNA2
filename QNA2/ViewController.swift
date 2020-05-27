7d735a87f7952e6da6e707118f4cdeee8658202a
Lydia-Lu-code <guipopo5@gmail.com>
2020-05-27 23:33:41 +0800

    Initial Commit

---
+++ QNA2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QNA2
+//
+//  Created by 維衣 on 2020/5/27.
+//  Copyright © 2020 Lydia-L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
